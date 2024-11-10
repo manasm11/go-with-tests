5c0d6227f1d58cd81d3534f5c508487cd9ddfa4b
Manas Mishra <manas@ManasMishra.>
2024-11-10 07:28:39 +0530

    wallet able to withdraw btc

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -3,14 +3,29 @@ package wallet
 import "testing"
 
 func TestWallet(t *testing.T) {
-  wallet := Wallet{}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-  wallet.Deposit(10)
+		wallet.Deposit(10)
 
-  got := wallet.Balance()
-  want := 10
+		got := wallet.Balance()
+		want := Bitcoin(10)
 
-  if got != want {
-    t.Errorf("got %d want %d", got, want)
-  }
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: 20}
+    wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
