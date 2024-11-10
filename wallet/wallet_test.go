17ee429b95a6dad562d1e0f45a3aa83b9b9db7c7
Manas Mishra <manas@ManasMishra.>
2024-11-10 08:13:16 +0530

    added check to insufficient balance

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -10,6 +10,13 @@ func TestWallet(t *testing.T) {
     }
   }
 
+  assertError := func (t testing.TB, err error)  {
+    t.Helper()
+    if err == nil {
+      t.Error("wanted an error but didn't get one")
+    }
+  }
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(10)
@@ -18,12 +25,20 @@ func TestWallet(t *testing.T) {
     assertBalance(t, wallet, want)
 	})
 
-
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: 20}
     wallet.Withdraw(Bitcoin(10))
 		want := Bitcoin(10)
 
     assertBalance(t, wallet, want)
+	})
+
+	t.Run("withdraw insufficient balance", func(t *testing.T) {
+		wallet := Wallet{balance: 20}
+		want := Bitcoin(20)
+    err := wallet.Withdraw(Bitcoin(100))
+
+    assertBalance(t, wallet, want)
+    assertError(t, err)
 	})
 }
