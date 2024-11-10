0bf7ac0bee5cb89cf6a0ff0a6f0d0ae278b0b515
Manas Mishra <manas@ManasMishra.>
2024-11-10 07:34:38 +0530

    refactored tests

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -3,29 +3,27 @@ package wallet
 import "testing"
 
 func TestWallet(t *testing.T) {
+
+  assertBalance := func (t testing.TB, w Wallet, want Bitcoin)  {
+    if w.balance != want {
+			t.Errorf("got %s want %s", w.balance, want)
+    }
+  }
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
-
 		wallet.Deposit(10)
-
-		got := wallet.Balance()
 		want := Bitcoin(10)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+    assertBalance(t, wallet, want)
 	})
 
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: 20}
     wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
 		want := Bitcoin(10)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+    assertBalance(t, wallet, want)
 	})
 }
