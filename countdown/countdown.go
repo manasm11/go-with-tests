62638fa2e312a0780c9d971f20faec59da388a5f
Manas Mishra <manas@ManasMishra.>
2024-11-12 08:19:49 +0530

    updated mocks in countdown

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -4,18 +4,30 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const finalWord = "Go!"
 const startCounter = 3
 
-func Countdown(w io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (s *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(w io.Writer, s Sleeper) {
 	for i := startCounter; i > 0; i-- {
 		fmt.Fprintln(w, i)
+    s.Sleep()
 	}
-	fmt.Fprint(w,finalWord)
+	fmt.Fprint(w, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	Countdown(os.Stdout, &DefaultSleeper{})
 }
--- countdown/countdown_test.go
+++ countdown/countdown_test.go
@@ -2,18 +2,51 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
+const (
+  sleep = "sleep"
+  write = "write"
+)
+
+type SpySleepPrinter struct {
+	Calls []string
+}
+
+func (ss *SpySleepPrinter) Sleep() {
+	ss.Calls = append(ss.Calls, sleep)
+}
+
+func (ss *SpySleepPrinter) Write([]byte) (int, error) {
+  ss.Calls = append(ss.Calls, write)
+  return 0, nil
+}
+
 func TestCountdown(t *testing.T) {
-  buffer := &bytes.Buffer{}
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
 
-  Countdown(buffer)
+		Countdown(buffer, &SpySleepPrinter{})
 
-  got := buffer.String()
-  want := "3\n2\n1\nGo!"
+		got := buffer.String()
+		want := "3\n2\n1\nGo!"
 
-  if got != want {
-    t.Errorf("got %q want %q", got, want)
-  }
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+  t.Run("sleep before every print", func(t *testing.T) {
+    spySleepPrinter := &SpySleepPrinter{}
+
+    Countdown(spySleepPrinter, spySleepPrinter)
+    
+    want := []string{write, sleep, write, sleep, write, sleep, write}
+
+    if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+      t.Errorf("got calls %v want %v", spySleepPrinter.Calls, want)
+    }
+  })
 }
