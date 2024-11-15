47ff94b1c4cf80546a1bfdb27405d6616ce577b3
Manas Mishra <manas@ManasMishra.>
2024-11-15 22:28:42 +0530

    created thread safe counter

---
+++ counter/counter.go
@@ -0,0 +1,16 @@
+package counter
+
+import "sync"
+
+type Counter struct {
+	mu sync.Mutex
+	v  int
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.v++
+}
+
+func (c *Counter) Value() int { return c.v }
